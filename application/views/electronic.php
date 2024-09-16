3e3e4a54b192d2d8a8d72d04e03a9075970629e6
Vaishnavi1135 <vaishnavirabade0110@gmail.com>
2024-09-16 16:21:06 +0530

    Update electronic.php

--- application/views/electronic.php
+++ application/views/electronic.php
@@ -3,6 +3,9 @@
 
 <h4 class="mb-3">ELECTRONIC APPLIENCES</h4>
 
+	<h4 class="mb-3">ELECTRONIC APPLIENCES</h4>
+	
+
 <div class="row g-5 mx-lg-0">
 <div class="col-md-6">
 <img class="img-fluid" src="<?php echo base_url();?>\assets\images\household.webp" alt="" style="height:300px; width:600px">
@@ -11,7 +14,7 @@
 
 <p>Are you looking for professional packers and movers? Moving your home with packers and movers can be a stressful experience. And if you don&#8217;t know how to safely pack and move electronics, things can quickly spiral out of control. When dealing with electronics, your packers and movers must use extreme caution.  After all, those devices are not only expensive, but they&#8217;re also delicate and can be easily damaged. However, if you plan ahead of time and follow some simple rules, your relocation with packers and movers should go off without a hitch.</p>
 
-<p>Hello</p>
+
 
 <p>Here are some tips on how to safely move electronics with <strong>packers and movers</strong>.</p>
 </div>

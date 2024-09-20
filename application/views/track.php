fcf8b285122b7663280073754a637ad68ae430f3
Vaishnavi1135 <vaishnavirabade0110@gmail.com>
2024-09-20 17:28:07 +0530

    commit

--- application/views/track.php
+++ application/views/track.php
@@ -5,7 +5,8 @@
                     <div class="container">
                         <div class="row justify-content-start" style="float:right; ">
                          <form action="" class="container" >
-                        <h1 style="color:white;">Tracking</h1>
+                        <h1 style="color:white;">Track your belongings with us!</h1>
+                        <h3 style="color:white;">To get the location updates of your consignment, write the Goods Consignment (GC) number in the box given below:</h3>
 
                         <label for="email"><b>Email</b></label>
                         <input type="text" placeholder="Enter Email" name="email"  required>

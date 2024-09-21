1469c9d8cf5a05fee85cac0d066b473e267c406d
Vaishnavi1135 <vaishnavirabade0110@gmail.com>
2024-09-21 12:01:56 +0530

    commit

--- application/views/contact.php
+++ application/views/contact.php
@@ -9,10 +9,10 @@
                 
                     
                 </div>
-                <div class="col-lg-6">
-                    <div class="bg-light text-center p-5 wow fadeIn" data-wow-delay="0.5s">
+                <div class="col-lg-6 mt-5">
+                    <div class="bg-light text-center p-5 wow fadeIn height:450px; mt-5;" data-wow-delay="0.5s">
                         <form>
-                            <div class="row g-3">
+                            <div class="row g-3 " style="height:450px;">
                                 <div class="col-12 col-sm-6">
                                     <input type="text" class="form-control border-0" placeholder="Your Name" style="height: 55px;">
                                 </div>

355434cd322747b3bb25c9939ba18004e5d53646
Vaishnavi1135 <vaishnavirabade0110@gmail.com>
2024-09-25 17:04:06 +0530

    commit

--- application/controllers/admin/Login.php
+++ application/controllers/admin/Login.php
@@ -112,7 +112,7 @@ class Login extends CI_Controller
             $data['created_by'] = $this->session->userdata('id');
             $res = $this->user_model->create($data);
             if($res){
-                $this->session->set_flashdata("Added successfully..!");
+                echo "added successfully";
             
             }
             

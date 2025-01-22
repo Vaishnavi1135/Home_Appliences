af50c90db9eb503ee7a444037bae72fc528ed740
Vaishnavi1135 <vaishnavirabade0110@gmail.com>
2025-01-22 17:15:33 +0530

    commit

--- application/views/admin/admin_template.php
+++ application/views/admin/admin_template.php
@@ -404,7 +404,7 @@
 
   <!-- Main Footer -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2024-2025 <a href="https://adminlte.io">KPM.io</a>.</strong>
+    <strong>Copyright &copy; 2024-2025 <a href="">KPM.io</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.2.0

e64ff9e7b03a72ce7e7515cd3a8ee6bcfcfeec3a
Vaishnavi1135 <vaishnavirabade0110@gmail.com>
2025-02-03 17:22:19 +0530

    commit

--- application/views/admin/Plans/list.php
+++ application/views/admin/Plans/list.php
@@ -3,6 +3,10 @@
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script> -->
 
+  
+        
+    
+
 
 <section class="content">
     <div class="container1">
--- application/views/admin/profile.php
+++ application/views/admin/profile.php
@@ -5,6 +5,8 @@
         <div class="row">
           <div class="col-md-3">
 
+          
+
             <!-- Profile Image -->
             <div class="card card-primary card-outline">
               <div class="card-body box-profile">
@@ -17,7 +19,7 @@
                 <h3 class="profile-username text-center">Nina Mcintire</h3>
 
                 <p class="text-muted text-center">Software Engineer</p>
-
+                
                 <ul class="list-group list-group-unbordered mb-3">
                   <li class="list-group-item">
                     <b>Followers</b> <a class="float-right">1,322</a>
@@ -35,6 +37,8 @@
               <!-- /.card-body -->
             </div>
             <!-- /.card -->
+
+           
 
             <!-- About Me Box -->
             <div class="card card-primary">
